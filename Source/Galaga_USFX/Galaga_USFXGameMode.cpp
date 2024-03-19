922b2f0625b627b9805348629b9df639d6496c35
Jhendef Replay <jheandef@gmail.com>
2024-03-19 10:39:06 -0400

    Generacion de naves aleatorias y la demostracion del uso practico del 'Tmap' realizada con exito (Aun no se implementa polimorfismo ni la documentacion desde el ultimo commit)

--- Source/Galaga_USFX/Galaga_USFXGameMode.cpp
+++ Source/Galaga_USFX/Galaga_USFXGameMode.cpp
@@ -28,8 +28,39 @@ void AGalaga_USFXGameMode::BeginPlay()
 {
 	Super::BeginPlay(); //Llama al metodo BeginPlay y se usar el 'Super' porque este es un metodo de AGameModeBase.
 	//Definimos las posiciones iniciales para las naves a spawnear.
+	TArray<TSubclassOf<AEnemy>> ClasesNaves;
+	TMap<TSubclassOf<AEnemy>, int> MaxNavesPorTipo;
+	ClasesNaves.Add(ANaveCaza::StaticClass());
+	ClasesNaves.Add(ACazaGen01::StaticClass());
+	ClasesNaves.Add(ACazaGen02::StaticClass());
+	ClasesNaves.Add(ANaveTransp::StaticClass());
+	ClasesNaves.Add(ATranspGen01::StaticClass());
+	ClasesNaves.Add(ATranspGen02::StaticClass());
+	ClasesNaves.Add(ANaveEspia::StaticClass());
+	ClasesNaves.Add(AEspiaGen01::StaticClass());
+	ClasesNaves.Add(AEspiaGen02::StaticClass());
+	ClasesNaves.Add(ANaveReab::StaticClass());
+	ClasesNaves.Add(AReabGen01::StaticClass());
+	ClasesNaves.Add(AReabGen02::StaticClass());
+	ClasesNaves.Add(ANaveNodriza::StaticClass());
 
-	FVector UIniNavesCaza = FVector(500.0f, 500.0f, 250.0f);
+	MaxNavesPorTipo.Add(ANaveCaza::StaticClass(), 5);
+	MaxNavesPorTipo.Add(ACazaGen01::StaticClass(), 5);
+	MaxNavesPorTipo.Add(ACazaGen02::StaticClass(), 5);
+	MaxNavesPorTipo.Add(ANaveTransp::StaticClass(), 5);
+	MaxNavesPorTipo.Add(ATranspGen01::StaticClass(), 5);
+	MaxNavesPorTipo.Add(ATranspGen02::StaticClass(), 5);
+	MaxNavesPorTipo.Add(ANaveEspia::StaticClass(), 5);
+	MaxNavesPorTipo.Add(AEspiaGen01::StaticClass(), 5);
+	MaxNavesPorTipo.Add(AEspiaGen02::StaticClass(), 5);
+	MaxNavesPorTipo.Add(ANaveReab::StaticClass(), 5);
+	MaxNavesPorTipo.Add(AReabGen01::StaticClass(), 5);
+	MaxNavesPorTipo.Add(AReabGen02::StaticClass(), 5);
+	MaxNavesPorTipo.Add(ANaveNodriza::StaticClass(), 1);
+
+
+
+	FVector UIniNavesCaza = FVector(0.0f, -1000.0f, 250.0f);
 
 	//No tocaremos la rotacion porque no nos interesa por ahora la rotacion de los objetos.
 	FRotator rotacionNave = FRotator(0.0f, 0.0f, 0.0f);
@@ -37,12 +68,19 @@ void AGalaga_USFXGameMode::BeginPlay()
 	UWorld* const World = GetWorld(); //Estamos obteniendo una direccion (puntero) de la clase UWorld (Escenario del juego).
 	if (World != nullptr) //Verificamos si el puntero obtenido es valido.
 	{
-		for (int i = 0; i < 5; i++) //Creamos un bucle para crear 5 naves.
-		{
-			FVector PActualNaveCaza = FVector(UIniNavesCaza.X, UIniNavesCaza.Y + i *150, UIniNavesCaza.Z); //Generamos un vector con las nuevas coordenadas.
-			ANaveCaza* NaveCazaInst = World->SpawnActor<ANaveCaza>(PActualNaveCaza, rotacionNave); //Creamos una instancia de la clase ANaveCaza.
-			Enemigos.Push(NaveCazaInst); //Agregamos la instancia a la lista de enemigos.
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 10; j++) {
+				TSubclassOf<AEnemy>ClaseNaveAleatoria = ClasesNaves[FMath::RandRange(0, ClasesNaves.Num()-1)];
+				if (MaxNavesPorTipo.Contains(ClaseNaveAleatoria)) {
+					int& NavesGeneradas = MaxNavesPorTipo[ClaseNaveAleatoria];
+					if (NavesGeneradas > 0) {
+						FVector PActualNaveCaza = FVector(UIniNavesCaza.X + i * 150, UIniNavesCaza.Y + j * 150, UIniNavesCaza.Z);
+						AEnemy* NaveCazaInst = World->SpawnActor<AEnemy>(ClaseNaveAleatoria, PActualNaveCaza, rotacionNave);
+						Enemigos.Push(NaveCazaInst);
+						NavesGeneradas--;
+					}
+				}
+			}
 		}
-		
 	}
 }
--- Source/Galaga_USFX/Galaga_USFXGameMode.h
+++ Source/Galaga_USFX/Galaga_USFXGameMode.h
@@ -10,6 +10,18 @@
 //Recordar no usar AEnemy por terminos de abstraccion.
 class AEnemy;
 class ANaveCaza;
+class ACazaGen01;
+class ACazaGen02;
+class ANaveTransp;
+class ATranspGen01;
+class ATranspGen02;
+class ANaveEspia;
+class AEspiaGen01;
+class AEspiaGen02;
+class ANaveReab;
+class AReabGen01;
+class AReabGen02;
+class ANaveNodriza;
 //UCLASS es un Macro de que delara que la clase siguente podra ser manipulada por el motor grafico.
 //Se le pasa el parametro 'MinimalAPI' en referencia al nivel de acceso de la API.
 UCLASS(MinimalAPI)
@@ -28,4 +40,16 @@ protected:
 public:
 	TArray<AEnemy*> Enemigos;
 	TArray<ANaveCaza*> NavesCaza;
+	TArray<ACazaGen01*> CazaGen01;
+	TArray<ACazaGen02*> CazaGen02;
+	TArray<ANaveTransp*> NavesTransp;
+	TArray<ATranspGen01*> TranspGen01;
+	TArray<ATranspGen02*> TranspGen02;
+	TArray<ANaveEspia*> NavesEspia;
+	TArray<AEspiaGen01*> EspiaGen01;
+	TArray<AEspiaGen02*> EspiaGen02;
+	TArray<ANaveReab*> NavesReab;
+	TArray<AReabGen01*> ReabGen01;
+	TArray<AReabGen02*> ReabGen02;
+	TArray<ANaveNodriza*> NavesNodriza;
 };
