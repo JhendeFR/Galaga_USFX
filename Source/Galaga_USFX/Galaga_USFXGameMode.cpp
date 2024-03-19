9e1f31493f421c8faddad2a2a5256980d7492af3
Jhendef Replay <jheandef@gmail.com>
2024-03-19 16:28:39 -0400

    Demostracion practica del Map documentada (Falta añadir funciones de eliminar y listar los objetos dentro del map)

--- Source/Galaga_USFX/Galaga_USFXGameMode.cpp
+++ Source/Galaga_USFX/Galaga_USFXGameMode.cpp
@@ -28,39 +28,47 @@ void AGalaga_USFXGameMode::BeginPlay()
 {
 	Super::BeginPlay(); //Llama al metodo BeginPlay y se usar el 'Super' porque este es un metodo de AGameModeBase.
 	//Definimos las posiciones iniciales para las naves a spawnear.
-	TArray<TSubclassOf<AEnemy>> ClasesNaves;
-	TMap<TSubclassOf<AEnemy>, int> MaxNavesPorTipo;
-	ClasesNaves.Add(ANaveCaza::StaticClass());
-	ClasesNaves.Add(ACazaGen01::StaticClass());
-	ClasesNaves.Add(ACazaGen02::StaticClass());
-	ClasesNaves.Add(ANaveTransp::StaticClass());
-	ClasesNaves.Add(ATranspGen01::StaticClass());
-	ClasesNaves.Add(ATranspGen02::StaticClass());
-	ClasesNaves.Add(ANaveEspia::StaticClass());
-	ClasesNaves.Add(AEspiaGen01::StaticClass());
-	ClasesNaves.Add(AEspiaGen02::StaticClass());
-	ClasesNaves.Add(ANaveReab::StaticClass());
-	ClasesNaves.Add(AReabGen01::StaticClass());
-	ClasesNaves.Add(AReabGen02::StaticClass());
-	ClasesNaves.Add(ANaveNodriza::StaticClass());
+	TArray<TSubclassOf<AEnemy>> TipoNaves;//Declaramos un Array dinamico de la clase proporcionada por UE.
 
-	MaxNavesPorTipo.Add(ANaveCaza::StaticClass(), 5);
-	MaxNavesPorTipo.Add(ACazaGen01::StaticClass(), 5);
-	MaxNavesPorTipo.Add(ACazaGen02::StaticClass(), 5);
-	MaxNavesPorTipo.Add(ANaveTransp::StaticClass(), 5);
-	MaxNavesPorTipo.Add(ATranspGen01::StaticClass(), 5);
-	MaxNavesPorTipo.Add(ATranspGen02::StaticClass(), 5);
-	MaxNavesPorTipo.Add(ANaveEspia::StaticClass(), 5);
-	MaxNavesPorTipo.Add(AEspiaGen01::StaticClass(), 5);
-	MaxNavesPorTipo.Add(AEspiaGen02::StaticClass(), 5);
-	MaxNavesPorTipo.Add(ANaveReab::StaticClass(), 5);
-	MaxNavesPorTipo.Add(AReabGen01::StaticClass(), 5);
-	MaxNavesPorTipo.Add(AReabGen02::StaticClass(), 5);
-	MaxNavesPorTipo.Add(ANaveNodriza::StaticClass(), 1);
+	//Usamos TMap para almacenar las clases de naves y la cantidad maxima de naves que se pueden generar.
+	TMap<TSubclassOf<AEnemy>, int> MaxNav;//TSubclassOf para asegurarse que podamos asignar subclases especificas solo de 'Enemy'.
+
+	//Añadimos las clases de naves al Array.
+	//Recordemos que al añadir pasamos las subclases y el medodo 'StaticClass' para obtener su clase (UClass) en le aejecucion.
+	//UClass nos sirve para crear, acceder y manipular a las propiedades y metodos de las clases en UE con C++ y Blueprints.
+	TipoNaves.Add(ANaveCaza::StaticClass());
+	TipoNaves.Add(ACazaGen01::StaticClass());
+	TipoNaves.Add(ACazaGen02::StaticClass());
+	TipoNaves.Add(ANaveTransp::StaticClass());
+	TipoNaves.Add(ATranspGen01::StaticClass());
+	TipoNaves.Add(ATranspGen02::StaticClass());
+	TipoNaves.Add(ANaveEspia::StaticClass());
+	TipoNaves.Add(AEspiaGen01::StaticClass());
+	TipoNaves.Add(AEspiaGen02::StaticClass());
+	TipoNaves.Add(ANaveReab::StaticClass());
+	TipoNaves.Add(AReabGen01::StaticClass());
+	TipoNaves.Add(AReabGen02::StaticClass());
+	TipoNaves.Add(ANaveNodriza::StaticClass());
+
+	//Añadimos cada clase de nave y la cantidad maximas que podemos generar.
+	//La estrutura seria: (ClaseNave, CantidadMaxima)=(llave, valor).
+	MaxNav.Add(ANaveCaza::StaticClass(), 5);
+	MaxNav.Add(ACazaGen01::StaticClass(), 5);
+	MaxNav.Add(ACazaGen02::StaticClass(), 5);
+	MaxNav.Add(ANaveTransp::StaticClass(), 5);
+	MaxNav.Add(ATranspGen01::StaticClass(), 5);
+	MaxNav.Add(ATranspGen02::StaticClass(), 5);
+	MaxNav.Add(ANaveEspia::StaticClass(), 5);
+	MaxNav.Add(AEspiaGen01::StaticClass(), 5);
+	MaxNav.Add(AEspiaGen02::StaticClass(), 5);
+	MaxNav.Add(ANaveReab::StaticClass(), 5);
+	MaxNav.Add(AReabGen01::StaticClass(), 5);
+	MaxNav.Add(AReabGen02::StaticClass(), 5);
+	MaxNav.Add(ANaveNodriza::StaticClass(), 1);
 
 
 
-	FVector UIniNavesCaza = FVector(0.0f, -1000.0f, 250.0f);
+	FVector UIniNaves = FVector(0.0f, -1000.0f, 250.0f);
 
 	//No tocaremos la rotacion porque no nos interesa por ahora la rotacion de los objetos.
 	FRotator rotacionNave = FRotator(0.0f, 0.0f, 0.0f);
@@ -68,16 +76,21 @@ void AGalaga_USFXGameMode::BeginPlay()
 	UWorld* const World = GetWorld(); //Estamos obteniendo una direccion (puntero) de la clase UWorld (Escenario del juego).
 	if (World != nullptr) //Verificamos si el puntero obtenido es valido.
 	{
+		//Numero de filas.
 		for (int i = 0; i < 3; i++) {
+			//Numero de Naves por fila.
 			for (int j = 0; j < 10; j++) {
-				TSubclassOf<AEnemy>ClaseNaveAleatoria = ClasesNaves[FMath::RandRange(0, ClasesNaves.Num()-1)];
-				if (MaxNavesPorTipo.Contains(ClaseNaveAleatoria)) {
-					int& NavesGeneradas = MaxNavesPorTipo[ClaseNaveAleatoria];
+				//Recordemos declarar a 'TipoNavAlea' como referencia de las subclases de 'AEnemy'.
+				TSubclassOf<AEnemy>TipoNavAlea = TipoNaves[FMath::RandRange(0, TipoNaves.Num()-1)];//Esto asigna una nave aleatoria de nuestro array de naves.
+				//Verificamos si el Tmap tiene la llave (TipoNavAlea) que recordemos que es una subclase especifica de 'Enemy'.
+				//Contains es un metodo que se llama en un TMap para verificar si contiene una clave específica (TipoNavAlea).
+				if (MaxNav.Contains(TipoNavAlea)) {
+					int NavesGeneradas = MaxNav[TipoNavAlea];//Asignanos la nave aleatoria como un entero a la variable 'NavesGeneradas'.
 					if (NavesGeneradas > 0) {
-						FVector PActualNaveCaza = FVector(UIniNavesCaza.X + i * 150, UIniNavesCaza.Y + j * 150, UIniNavesCaza.Z);
-						AEnemy* NaveCazaInst = World->SpawnActor<AEnemy>(ClaseNaveAleatoria, PActualNaveCaza, rotacionNave);
-						Enemigos.Push(NaveCazaInst);
-						NavesGeneradas--;
+						FVector PActualNaves = FVector(UIniNaves.X + i * 150, UIniNaves.Y + j * 105, UIniNaves.Z);//Esto solo controla la distancia entre las naves
+						AEnemy* NavesInst = World->SpawnActor<AEnemy>(TipoNavAlea, PActualNaves, rotacionNave);//Esto spawnea las naves en el mundo.
+						Enemigos.Push(NavesInst);//Esto añade las naves al array de enemigos.
+						NavesGeneradas--;//Cada vez que se crea una nave, se reduce el contador de naves disponibles para ese tipo.
 					}
 				}
 			}
